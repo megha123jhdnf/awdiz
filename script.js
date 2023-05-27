749067e2338ce80465d942353be48a67999c8ae3
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-05-27 11:51:41 +0530

    update

---
+++ script.js
@@ -0,0 +1,34 @@
+// Js -datatypes -String,number,Boolean
+
+// String -text -'1234' ,"lenovo",`hi`
+// Number -number - 1234,0.12,4522522.0
+// Boolean -true , false
+
+
+// variable- we can store the data
+
+
+//  string
+
+var kuchbhi='awdiz';
+console.log(kuchbhi);
+console.log(typeof(kuchbhi));
+
+//   var kuchbhi='1234';
+//   console.log("data here-", kuchbhi);
+//  console.log("datatype here-",typeof(kuchbhi));
+
+ // number
+//   var kuchbhi=1234;
+//   console.log("data here-", kuchbhi);
+//   console.log("datatype here-",typeof(kuchbhi));
+
+ // Boolean
+//   var booleanvalue='false';
+//   console.log("data here-",booleanvalue );
+//   console.log("datatype here-",typeof(booleanvalue));
+
+//  var booleanvalue=false;
+// console.log("data here-",booleanvalue );
+//  console.log("datatype here-",typeof(booleanvalue));
+
