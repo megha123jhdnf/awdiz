7285728f0d704c0bfe0cf9c2953578782bac4735
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-05-27 10:59:22 +0530

    update

---
+++ day 14 script.js
@@ -0,0 +1,23 @@
+ var kuchbhi="meghali";
+ console.log(kuchbhi);
+ console.log(typeof(kuchbhi));
+
+
+ var kuchbhi=1234;
+ console.log("datatype here"-kuchbhi);
+ console.log("datatype here"-typeof(kuchbhi));
+
+ var kuchbhi='1234';
+ console.log("data here" -kuchbhi);
+ console.log("datatype here"-typeof(kuchbhi));
+
+
+
+ var kuchbhi='true';
+ console.log(kuchbhi);
+ console.log(typeof(kuchbhi));
+
+ var kuchbhi=true;
+ console.log(kuchbhi);
+ console.log(typeof(kuchbhi));
+
