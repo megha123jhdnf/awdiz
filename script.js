c3b6e009b663ddd9ba4872213307822fc6c18f98
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-05-27 22:28:37 +0530

    update

--- script.js
+++ script.js
@@ -10,25 +10,22 @@
 
 //  string
 
-var kuchbhi='awdiz';
-console.log(kuchbhi);
-console.log(typeof(kuchbhi));
+// var kuchbhi='awdiz';
+// console.log(kuchbhi);
+// console.log(typeof(kuchbhi));
 
 //   var kuchbhi='1234';
 //   console.log("data here-", kuchbhi);
-//  console.log("datatype here-",typeof(kuchbhi));
+//  console.log("datatype here-",typeof(clearkuchbhi));
 
  // number
 //   var kuchbhi=1234;
 //   console.log("data here-", kuchbhi);
 //   console.log("datatype here-",typeof(kuchbhi));
 
- // Boolean
-//   var booleanvalue='false';
-//   console.log("data here-",booleanvalue );
-//   console.log("datatype here-",typeof(booleanvalue));
+ Boolean
+  var booleanvalue='false';
+  console.log("data here-",booleanvalue );
+  console.log("datatype here-",typeof(booleanvalue));
 
-//  var booleanvalue=false;
-// console.log("data here-",booleanvalue );
-//  console.log("datatype here-",typeof(booleanvalue));
 
---
+++ script1.js
@@ -0,0 +1,98 @@
+var number1 = 10;
+var number2= 20;
+
+// var  add =number1 + number2;
+// console.log( add );
+
+// var  sub =number1 - number2;
+// console.log( sub);
+
+// var  mul =number1 *number2;
+// console.log( mul );
+
+// var  div =number1 + number2;
+// console.log( div );
+
+
+// var i=1;
+// console.log(i);
+// i= i+1;
+// console.log(i);
+
+
+// var num = 150;
+// console.log(num >200);
+
+
+// if else
+
+//     if (conditions) {
+//         code to be execute
+//         console.log("condition true")
+//     } else if (condtions second) {
+//         code to be execute
+//     console.log("condition true")
+//     }  else if (condtions second) {
+//             code to be execute
+//         console.log("condition true")
+//     }else {
+//         code to be execute
+//     console.log("condition false")
+//     }
+
+
+// Q. Check given number is lesser than 100 or 150 or 200;
+
+// var num = 98763;
+// if (num<100){
+//     console.log("number is greater clearthan 100");
+// }
+// else if (num<150){
+//     console.log("number is than 150 and greater than 100");
+
+// }
+//  else {
+//     console.log("Number is not Lesser than 100 nor 150")
+// }
+
+
+// Q. Check user age and provied licennnn... 
+
+// if user age is 18 then he is applicbke for Learnign licc... 
+// user age is greater than 18 and lesser than 80 then dl 
+// user age is lesser 18 or greter 80 not allowed dl  
+
+// var userAge =25;
+// if(userAge==18){
+//     console.log("You are applicable for Learning Licence...")
+
+// }
+// else if(userAge < 40 && userAge >18){
+//     console.log("You are allow for Driving Licence..")
+// }
+// else{
+//     console.log("You are not  allow for driving licence.. ")
+// }
+
+
+// var num1 = 120;
+// var num2 = 120;
+// console.log(num1 == num2);
+// // ==here only check value
+
+// var num1 = 120;
+// var num2 = '120';
+// console.log(num1 === num2);
+// // check value and datatype
+
+
+// here we use && (and) || (or)
+var num =95;
+// console.log(num > 50 && num < 99 && num >45);
+// console.log(num > 50 || num < 99 || num <45);
+// console.log(num > 50 || num < 90 || num< 20);
+
+
+
+
+
