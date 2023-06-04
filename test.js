9589720c82ab7332a1884052acd0819614059f20
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-06-04 16:48:30 +0530

    test

---
+++ test.js
@@ -0,0 +1,49 @@
+//  var array =[2,3,55,6,67,78,23,5,67,78,43,67,34,6,74,7,45,23,45,78,89,34,23]
+//  var target = 54;
+ 
+//  function findNumber(array,target){
+//       var flag = false;
+//       for(i <0; i<array.length -4;i++){
+
+//       for(j = i+1; j<array.length -3;j++){
+//         for(k = j+1; k<array.length -2;k++){
+//             for(l = k+1; l<array.length -1;l++){
+//                 for(m = l+1; m<array.length ;m++){
+
+//                     if (array[i] + array[j] + array[k] + array[l] + array[m] ==target){
+//                         console.log(array[i]),array[j],array[k],array[l],array[m]
+//                         flag = true;
+
+//                     }
+//                 }
+//             }
+
+//         }
+//         }
+//     }
+
+//     if (flag == false){
+//         console.log(" addition of three number is not possible")
+//     }
+
+// findNumbers(array,target) ;
+
+// }
+
+
+
+2.
+var array2=[2,3,55,6,67,78,23,5,67,78,43,67,34,6,74,7,45,23,45,78,89,34,23];
+var target2= 236028;
+
+
+ function multiplication(){
+     var mul= array2 * target2;
+       console.log(mul,"multiplication")
+
+
+}
+multiplication()
+
+      
+ 
