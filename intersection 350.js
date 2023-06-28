217e6b0e45587cd40652a997f829bb218bc574db
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-06-28 16:14:28 +0530

    Update intersection 350.js

--- intersection 350.js
+++ intersection 350.js
@@ -1,21 +1,26 @@
- const nums1 = [1,2,2,1]
- const nums2 = [2,2]
-//   Output: [2]
+//  const nums1 = [1,2,2,1]
+//  const nums2 = [2,2]
+// //   Output: [2,2]
 
 
-// const nums1=[4,9,5]
-// const nums2=[9,4,9,8,4]
-// // Output:[9,4]
-// [4,9]
+const nums1=[4,9,5]
+const nums2=[9,4,9,8,4]
+//  Output:[9,4]
+//  [4,9]
 
 
 
 function intersection(nums1,nums2){
     return nums1.filter(Element => nums2.includes(Element));
-}
+}         
 
 const intersectionArray = intersection(nums1,nums2);
 console.log(intersectionArray)
+
+
+
+
+
 
 
 
